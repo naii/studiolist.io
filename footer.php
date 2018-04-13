c757815ea5e5d00c6b0db6be37caf696375f8d23
Alexander Kluge <wunderkind@wunderkind.local>
2018-04-13 23:04:03 +0200

    Launch and initial commit
    /done yesterday I declared a pause of #studiolist and today I coded for
    hours (HTML, CSS, PHP) to launch a simple version of Studio List.
    Intentionally, I left out graphical design elements because when I
    worked on them in the past I never actually shipped something. So, I’m
    staring with a plain-text version I like how fast it loads and pure it
    looks.

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+<footer>
+	<hr class="below-the-fold call-to-action centered">
+	<p class="small centered">served by <a href="https://naii.io">naii.io</a></p>
+	<p>Studio List is <a href="https://2017.ind.ie/ethical-design/">ethical by design</a>, made to empower vocal artists living a lifestyle of work and travel, while having a cause in life <em>that is more ingenious than power and money</em>, and nourishing their connection to the world.</p>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Studio List - recording studios for voice artists</title>
+	<meta charset="utf-8">
+	<meta http-equiv="language" content="en">
+	<meta http-equiv="content-language" content="en">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
